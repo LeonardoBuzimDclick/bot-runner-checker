b80f4ff15b4f7bccb5104e95d3256f0f733aa0f8
LeonardoBuzimDclick <134735951+LeonardoBuzimDclick@users.noreply.github.com>
2025-03-21 09:48:46 -0300

    Update main.py
    main v2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import psutil
 import subprocess
 import os
 import datetime
+import time
 
 PROCESSO = ["ElectroNeek-loader.exe", "ElectroNeek Bot Runner", "ElectroNeek Bot Runner.exe"]
 CAMINHO_EXE = r"C:\Program Files\ElectroNeek Robot\Loader\ElectroNeek-loader.exe"
@@ -13,6 +14,16 @@ def programa_esta_rodando(nomes_processos):
             return True
     return False
 
+def processo_esta_realmente_ativo(nomes_processos):
+    for processo in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info']):
+        if processo.info['name'] in nomes_processos:
+            # Espera um momento e pega o uso de CPU novamente
+            time.sleep(1)  
+            cpu_uso = processo.cpu_percent(interval=1)
+            
+            if cpu_uso > 0:  # Se estiver usando CPU, então está realmente ativo
+                return True
+    return False
 
 def registrar_log_erro(mensagem):
     data_atual = datetime.datetime.now().strftime('%Y-%m-%d')
@@ -37,7 +48,8 @@ def registrar_ultima_execucao(status):
 
 def executar_como_admin(caminho_exe):
     try:
-        subprocess.run(["powershell", f'Start-Process "{caminho_exe}" -Verb RunAs'], shell=True, check=True)
+        subprocess.run(["powershell", f'Start-Process "{caminho_exe}"'], shell=True, check=True)
+        #subprocess.run(["runas", "/user:Administrador", caminho_exe], shell=True, check=True)
         registrar_ultima_execucao("Ativo - Executado com sucesso")
     except subprocess.CalledProcessError as e:
         registrar_log_erro(f"Erro ao iniciar o programa: {e}")
@@ -45,7 +57,8 @@ def executar_como_admin(caminho_exe):
         registrar_log_erro(f"Erro inesperado: {e}")
 
 
-if not programa_esta_rodando(PROCESSO):
+#if not programa_esta_rodando(PROCESSO):
+if not processo_esta_realmente_ativo(PROCESSO):
     registrar_historico()
     executar_como_admin(CAMINHO_EXE)
 else:
